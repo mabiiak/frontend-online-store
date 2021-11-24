1e59078e1005192310a1ee88ffdafd0bd2f7a25e
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-11-24 19:06:27 -0300

    Requisito 8 - Adiciona produtos a partir da tela de listagem

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends Component {
-  constructor() {
-    super();
-
-    this.saveCartItems = this.saveCartItems.bind(this);
-  }
-
-  saveCartItems() {
-    const sendProductToLocalStorage = (cartItems) => {
-      localStorage.setItem('cartItemKey', JSON.stringify(cartItems));
-    };
-    return sendProductToLocalStorage;
-  }
-
   render() {
+    const { onClick, cardProduct } = this.props;
     return (
       <div>
         <button
           data-testid="product-add-to-cart"
           type="button"
-          onClick={ this.saveCartItems }
+          onClick={ () => onClick(cardProduct) }
         >
           Adicionar ao carrinho
         </button>
@@ -29,4 +18,11 @@ class Button extends Component {
   }
 }
 
+Button.propTypes = {
+  cardProduct: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 export default Button;
+
+// Ajuda do João Oliveira Avelino no requisito 8
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Cart from '../pages/Cart';
 import Home from '../pages/Home';
+import Product from '../pages/Product';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ Cart } />
+          <Route exact path="/product/:id" component={ Product } />
         </Switch>
       </main>
     );
