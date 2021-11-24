b84371dc387a3916e31022a1688acb35479f8953
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-11-24 08:55:38 -0300

    Requisito 7 - falhou no avaliador, novo push

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ export async function getCategories() {
   const requestCategories = await fetch(url);
   const requestResponse = await requestCategories.json();
   return requestResponse;
-  // console.log(requestResponse);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
